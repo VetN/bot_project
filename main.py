a24215981794af98ea7bb5e617eae93a8b7c2900
VetN <122635397+VetN@users.noreply.github.com>
2023-04-06 11:02:13 +0300

    разместила класс

--- main.py
+++ main.py
@@ -18,6 +18,28 @@ class ConvertionException(Exception):
     print("Неправильный ввод данных")
     #pass
 
+class Exchange:
+
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f"Нельзя вводить одинаковые валюты {base}.")
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {quote}")
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {base}")
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f"Не удалось обработать запрос {amount}")
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base
 
 @bot.message_handler(commands=['start', 'help'])
 def start_exchange(message: telebot.types.Message):
@@ -38,27 +60,13 @@ def convert(message: telebot.types.Message):
             #("Неправильный ввод данных")
 
     quote, base, amount = values
-    if quote == base:
-        raise ConvertionException(f"Нельзя вводить одинаковые валюты {base}.")
-    try:
-        quote_ticker = keys[quote]
-    except KeyError:
-        raise ConvertionException(f"Не удалось обработать валюту {quote}")
-    try:
-        base_ticker = keys[base]
-    except KeyError:
-        raise ConvertionException(f"Не удалось обработать валюту {base}")
-    try:
-        amount = float(amount)
-    except ValueError:
-        raise ConvertionException(f"Не удалось обработать запрос {amount}")
+    total_base = Exchange.convert(quote, base, amount)
 
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-    total_base = json.loads(r.content)[keys[base]]
+
     text = f'Цена {amount} {quote} к {base} -{total_base}'
     bot.send_message(message.chat.id, text)
 
-
-
-
 bot.polling()
+
+
+
